206d4840688baaf55c99eb379321ea0cb75e51a9
Pedro Oliveira <pedro.brandao.oliveira@gmail.com>
2023-10-01 04:14:50 +0100

    📅 feat: sequelize update
    - addition of database type to env example, config and database

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv"
 
 dotenv.config()
 
-const { DISCORD_TOKEN, APPLICATION_ID, GUILD_ID, DB_NAME, DB_USER, DB_PASS, DB_HOST  } = process.env;
+const { DISCORD_TOKEN, APPLICATION_ID, GUILD_ID, DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_TYPE  } = process.env;
 
 switch(true) {
     case !DISCORD_TOKEN:
@@ -12,13 +12,15 @@ switch(true) {
     case !GUILD_ID:
         throw new Error("Please provide a valid Discord guild ID.");
     case !DB_NAME:
-        throw new Error("Please provide a valid database name.");
+        throw new Error("Please provide a valid database name. (gumshoe, wordpress, etc.)");
     case !DB_USER:
-        throw new Error("Please provide a valid database user.");
+        throw new Error("Please provide a valid database user. (dbuser, postgres, etc.)");
     case !DB_PASS:
         throw new Error("Please provide a valid database password.");
     case !DB_HOST:
-        throw new Error("Please provide a valid database host.");
+        throw new Error("Please provide a valid database host. (localhost,192.168.0.200, etc.)");
+    case !DB_TYPE:
+        throw new Error("Please provide a valid database type. (mysql, postgres, etc.)");
 }
 
 export const config = {
@@ -28,5 +30,6 @@ export const config = {
     DB_NAME,
     DB_USER,
     DB_PASS,
-    DB_HOST
+    DB_HOST,
+    DB_TYPE
 };
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
+import { Dialect } from "sequelize/types";
 import { config } from '../config';
 
 const seql = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASS, {
-    dialect: 'mysql',
+    dialect: config.DB_TYPE as Dialect,
     host: config.DB_HOST,
     logging: false,
 })
